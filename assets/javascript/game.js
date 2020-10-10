dd0366522a09360a98165fdcf5bbb3e0dc8f7e7d
koa2019 <no-reply>
2020-10-09 19:33:54 -0700

    render values of number of losses & guesses left to index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,24 @@ $(document).ready(function () {
 
     // variables
     let wins = 0
-    let losses = 0
+    let loss = 0
     let numGuess = 3
     let guess
 
     // define functions
     function renderWins() {
-        console.log(wins)
-        document.querySelector('#wins').innerHTML = wins
+        document.querySelector('#wins').textContent = wins
     }
-
+    function renderLosses() {
+        document.querySelector('#loss').textContent = loss
+    }
+    function renderNumGuess() {
+        document.querySelector('#numGuess').textContent = numGuess
+    }
 
     // call functions
     renderWins()
+    renderLosses()
+    renderNumGuess()
 
 })
