7479e4c190925fee79268ab60e7ce2111cee47a0
koa2019 <no-reply>
2020-10-09 19:47:30 -0700

    created function to pick a random letter from alphabet. Created eventListener to capture & pass user's guess to a function that renders it the browser

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     let guess
 
     // define functions
+    // functions render values to browser
     function renderWins() {
         document.querySelector('#wins').textContent = wins
     }
@@ -16,10 +17,34 @@ $(document).ready(function () {
     function renderNumGuess() {
         document.querySelector('#numGuess').textContent = numGuess
     }
+    function renderUserGuess(){
+        document.querySelector('#userGuess').textContent = guess
+    }
+
+    // function picks random letter from alphabet
+    // site: https://www.coderrocketfuel.com/article/generate-a-random-letter-from-the-alphabet-using-javascript
+    function getRandomLetter() {
+        const alphabet = "abcdefghijklmnopqrstuvwxyz"
+        const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
+        return randomCharacter
+    }
+
+    
+
 
     // call functions
     renderWins()
     renderLosses()
     renderNumGuess()
+    renderUserGuess()
+    let randLetter = getRandomLetter()
+    console.log(randLetter)
+
+    // event listener to capture user's input 
+    document.onkeyup = function(e){
+        guess = e.key
+        renderUserGuess(guess)
+    }
+
 
 })
