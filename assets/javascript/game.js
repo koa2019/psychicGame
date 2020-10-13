740fb938167ac5276ea7534e6372ae9c25a3f06a
koa2019 <no-reply>
2020-10-12 18:51:58 -0700

    fixed extra loop iteration for number of guesses by reducing numGuess before the loop starts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,15 @@ $(document).ready(function () {
     let wins = 0
     let loss = 0
     let numGuess = 3
-    let guess
-    let gameOverMessage = "Oh-no! You're out of guesses"
+    let userGuess = []
+    let randLetter = null
+    let guess = null
+    const winnerMessage = "Winner Winner Chicken Dinner!"
+    let gameOverMessage = "Game Over! You're out of guesses"
 
-    // define functions
-    // functions render values to browser
+    // +++++++ FUNCTION DEFINTIONS ++++++++++
+
+    // functions render various varible's values to browser when called
     function renderWins() {
         document.querySelector('#wins').textContent = wins
     }
@@ -19,62 +23,76 @@ $(document).ready(function () {
         document.querySelector('#numGuess').textContent = numGuess
     }
     function renderUserGuess() {
-        document.querySelector('#userGuess').textContent = guess
+        document.querySelector('#userGuess').textContent = userGuess
     }
-    function renderZeroGuess() {
-        document.querySelector("#status").textContent = gameOverMessage
-        console.log("hit numGuess conditional")
+
+    // function renders winner message
+    function renderWinnerMessage(){
+        alert(winnerMessage)
     }
+
+    // functions renders Game Over message
+    function renderGameOverMessage() {
+        alert(gameOverMessage)
+    }
+
     // function picks random letter from alphabet
     // site: https://www.coderrocketfuel.com/article/generate-a-random-letter-from-the-alphabet-using-javascript
     function getRandomLetter() {
         const alphabet = "abcdefghijklmnopqrstuvwxyz"
-        const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
-        return randomCharacter
+        randLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
+
     }
 
-
-
-
-    // call functions
-    renderWins()
-    renderLosses()
-    renderNumGuess()
-    renderUserGuess()
-    let randLetter = getRandomLetter()
-    console.log(randLetter)
-
-
-
-    // event listener to listen for a keyboard input
-    document.onkeyup = function (e) {
-
-        // assigns keyboard input to guess variable
-        guess = e.key
-        guess.toLowerCase()
-
-
-        // checks to make sure user has enough guesses to play
-        if (numGuess === 0) {
-            return renderZeroGuess()
-        }
-        else {
-            renderUserGuess(guess)
-
-            if (guess === randLetter) {
-                wins++
-                alert('winner')
-            }
-            else {
-                loss++
-                numGuess--
-            }
-        }
+    // function resets game, but keeps number of wins & losses
+    function resetGame() {
+        numGuess = 3
+        userGuess = []
         renderWins()
         renderLosses()
         renderNumGuess()
-
-
+        renderUserGuess()
+        getRandomLetter()
+        console.log('New Game. Letter to guess ' + randLetter)
     }
 
-})
+    // call functions when page loads
+    renderNumGuess()
+    getRandomLetter()
+    console.log('Letter to guess ' + randLetter)
+
+
+    // event listener for a keyboard input from user
+    document.onkeyup = function (e) {
+
+        numGuess--
+        renderNumGuess()
+
+        // assigns keyboard input to guess variable
+        guess = e.key.toLowerCase()
+
+        // render user guess to browswer
+        userGuess.push(guess)
+        console.log(userGuess)
+
+        if (guess === randLetter) {
+            wins++
+            renderWinnerMessage()
+            renderWins()
+            resetGame()
+        }
+        else {
+            renderWins()
+            renderLosses()
+            renderUserGuess()
+        }
+
+        if (numGuess === 0) {
+            loss++
+            renderGameOverMessage()
+            resetGame()
+        }
+
+    } // ends.onkeyup()
+}) // ends document.ready()
+
